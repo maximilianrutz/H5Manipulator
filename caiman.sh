62132b7289f196d15ceffcb5a68859184ac94502
maxtue <47662908+maxtue@users.noreply.github.com>
2019-03-22 08:31:37 +0100

    Update caiman.sh
    Exec new container by its id

--- caiman.sh
+++ caiman.sh
@@ -3,4 +3,4 @@
 sensible-browser #192.168.99.100:8888
 docker rm -f $(docker ps -a -q)
 docker-compose up -d
-docker exec -ti dockertoolbox_neurophys_1 bash -c "cd src && pwd && source activate caiman && jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token=''"
+docker exec -ti $(docker ps -l -q) bash -c "cd src && pwd && source activate caiman && jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token=''"
