aeea65d21023c286fda56e3a877702459dcccccd
Maximilian Rutz <maximilian.rutz@student.uni-tuebingen.de>
2020-04-16 15:57:35 +0200

    before removing ffmpeg path

--- data-analysis/GUI.py
+++ data-analysis/GUI.py
@@ -1,9 +1,10 @@
 """
-TODO: 
+TODO:
 - include ffmpeg into executable
 - return to mainloop when load data is cancelled without asking for save data
 - dropdown menu for convert to file endings
 - convert all files of a directory to chosen format: https://stackoverflow.com/questions/42438380/ffmpeg-in-python-script
+- default open last used directory when loading file
 
 how to make executables with pyinstaller
 
@@ -15,17 +16,14 @@ on Mac: pyinstaller
 --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl'
 GUI.py
 
-on Windows: pyinstaller
---onefile
---windowed
---add-binary ".\dependencies\windows\ffmpeg\bin\ffmpeg.exe;."
---icon=icons/mac.png
-GUI.py
+on Windows: 
+pyinstaller --onefile --add-binary=".\ffmpeg.exe;." GUI.py
 """
 
 import tkinter as tk
 from tkinter import filedialog as fd
 import os
+import sys
 
 
 class GUI:
@@ -34,14 +32,14 @@ class GUI:
     def set_variables(self):
         self.gui_height = 100
         self.gui_width = 200
-        self.filedir_path = os.path.dirname(os.path.abspath(__file__))
         self.set_path_to_ffmpeg()
 
     def set_path_to_ffmpeg(self):
-        if True:
-            self.convert_ffmpeg_path = r".\ffmpeg.exe"
-        else:
-            self.convert_ffmpeg_path = self.filedir_path + r"\dependencies\windows\ffmpeg\bin\ffmpeg.exe"
+        try:
+            base_path = sys._MEIPASS
+        except AttributeError:
+            base_path = os.path.abspath(".")
+        self.convert_ffmpeg_path = os.path.join(base_path, "ffmpeg.exe")
 
     def define_gui_elements(self):
         self.root.title("File Converter")
@@ -62,8 +60,9 @@ class GUI:
     def convert_file(self):
         self.load_file()
         self.save_file()
-        self.convert_flag = f" -i {self.load_filename} {self.save_filename}"
-        self.convert_command = self.convert_ffmpeg_path + self.convert_flag        
+        self.convert_flag = f'" -i {self.load_filename} {self.save_filename}"'
+        self.convert_command = self.convert_ffmpeg_path# + self.convert_flag
+        print(self.convert_command)
         os.system(self.convert_command)
 
     def activate_gui_elements(self):
