242a86e120030d1f0786613a62b66db66fee3840
Maximilian Rutz <maxfrbln@gmail.com>
2020-05-01 17:33:46 +0200

    adding text and image fields

--- gui/code/gui.py
+++ gui/code/gui.py
@@ -2,15 +2,11 @@
 TODO:
 -  read avi save as multitiff via .py, --windowed, extra box in gui for output
 
-TODISCUSS:
-- set --windowed flag?
-- open hdf file tree?
-
 DONETODAY:
 - mp4, avi
 - open h5 and multitiff
 - h5py automatically detect video files
-- tickbox with names 
+- tickbox with names
 - if size(dataset) == 1: save one file
 - h5 files a lot of small files - save as multitiffs
 
@@ -30,67 +26,109 @@ pyinstaller --onefile --add-binary="dependencies\windows\ffmpeg.exe;." --icon=de
 
 import tkinter as tk
 from tkinter import filedialog as fd
-import os
-import sys
+import tkinter.scrolledtext as tkst
+
+import numpy as np
+import cv2
+from PIL import Image, ImageTk
 
 
 class Gui:
     """ Initialization """
 
-    def set_variables(self):
-        self.gui_height = 100
-        self.gui_width = 200
-        self.set_path_to_ffmpeg()
-
-    def set_path_to_ffmpeg(self):
-        try:
-            self.convert_ffmpeg_path = os.path.join(sys._MEIPASS, "ffmpeg.exe")
-        except AttributeError:
-            self.convert_ffmpeg_path = "ffmpeg"
-
-    def define_gui_elements(self):
-        self.root.title("File Converter")
-        self.canvas = tk.Canvas(self.root, height=self.gui_height, width=self.gui_width)
-        self.frame = tk.Frame(self.root)
-        self.convert_button = tk.Button(
-            self.frame, text="Convert file", command=self.convert_file
+    def __init__(self):
+        gui_height = 400
+        gui_width = 800
+        self.tk_root = tk.Tk()
+        self.tk_root.title("Data Converter Neurophysiology Tuebingen")
+        self.tk_canvas = tk.Canvas(self.tk_root, height=gui_height, width=gui_width)
+        self.tk_frame = tk.Frame(self.tk_root)
+        self.tk_load_button = tk.Button(
+            self.tk_frame, text="Load file", command=self.load_video
+        )
+        self.tk_save_button = tk.Button(
+            self.tk_frame, text="Save file", command=self.save_video
+        )
+        self.tk_printout = tkst.ScrolledText(
+            self.tk_frame,
+            wrap=tk.WORD,
+            width=int(0.5 * gui_width),
+            height=int(0.5 * gui_height),
         )
 
-    """ Main program elements """
+        self.frames = []
 
-    def load_file(self):
-        self.load_filename = fd.askopenfilename()
+    def load_video(self):
+        filename = fd.askopenfilename()
+        filetype = filename.split(".")[1]
+        if filetype in ["avi", "mp4"]:
+            self.load_frames(filename)
+        elif filetype in ["h5", "hdf5"]:
+            pass
+        else:
+            print(f"Unknown file type of file {filename}")
 
-    def save_file(self):
-        self.save_filename = fd.asksaveasfilename()
+    def load_frames(self, filename):
+        self.tk_printout.place(relx=0, rely=1, anchor="sw")
 
-    def convert_file(self):
-        self.load_file()
-        if not self.load_filename:
-            return
-        self.save_file()
-        if not self.save_filename:
-            return
-        self.convert_flag = f" -i {self.load_filename} -codec copy {self.save_filename}"
-        self.convert_command = self.convert_ffmpeg_path + self.convert_flag
-        os.system(self.convert_command)
+        cap = cv2.VideoCapture(filename)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+        self.frames = np.empty(
+            (frame_count, frame_height, frame_width, 3), np.dtype("uint8")
+        )
+        frame_counter = 0
+        ret = True
+        while frame_counter < frame_count and ret:
+            self.tk_printout.insert(
+                tk.INSERT, f"Loading frame {frame_counter + 1}/{frame_count}\n"
+            )
+
+            ret, frame = cap.read()
+            self.frames[frame_counter] = frame
+
+            img = ImageTk.PhotoImage(image=Image.fromarray(frame))
+            self.tk_canvas.create_image(50, 50, image=img, anchor="se")
+            frame_counter += 1
+        cap.release()
+
+    def save_video(self):
+        filename = fd.asksaveasfilename()
+        filetype = filename.split(".")[1]
+        if filetype == "avi":
+            self.save_avi(filename)
+        elif filetype in ["tif", "tiff"]:
+            self.save_tiff(filename)
+
+    def save_avi(self, filename):
+        fourcc = cv2.VideoWriter_fourcc(*"XVID")
+        out = cv2.VideoWriter(filename, fourcc)
+        for frame in self.frames:
+            out.write(frame)
+        out.release()
+
+    def save_tiff(self, filename):
+        imlist = []
+        for frame in self.frames:
+            imlist.append(Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)))
+        imlist[0].save(
+            filename,
+            compression="tiff_deflate",
+            save_all=True,
+            append_images=imlist[1:],
+        )
 
     def activate_gui_elements(self):
-        self.canvas.pack()
-        self.frame.place(relwidth=1, relheight=1)
-        self.convert_button.place(relx=0.5, rely=0.5, anchor="center")
-
-    """Execution as script"""
+        self.tk_canvas.pack()
+        self.tk_frame.place(relwidth=1, relheight=1)
+        self.tk_load_button.place(relx=0, rely=0, anchor="nw")
+        self.tk_save_button.place(relx=1, rely=0, anchor="ne")
 
     def main(self):
-        self.set_variables()
-        self.run()
-
-    def run(self):
-        self.root = tk.Tk()
-        self.define_gui_elements()
         self.activate_gui_elements()
-        self.root.mainloop()
+        self.tk_root.mainloop()
 
 
 if __name__ == "__main__":
