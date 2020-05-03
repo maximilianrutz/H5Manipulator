8410e3407e21d3f4495ee100b0247ec2f8da0582
Maximilian Rutz <maxfrbln@gmail.com>
2020-05-03 11:54:41 +0200

    reorganized repository

--- caiman/linux_caiman.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker rm -f $(docker ps -a -q)
-docker-compose up -d
-xdg-open 'http://localhost:8888'
-docker exec -ti $(docker ps -l -q) /bin/bash -c "export MKL_NUM_THREADS=1 && export OPENBLAS_NUM_THREADS=1 &&\
-cd /root/caiman_data && pwd && source activate caiman && jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token=''"
